a07966941536f7be56a8073226e4e95f0aa8afaf
Jheremy <67889063+jkcraftdom@users.noreply.github.com>
2022-11-24 11:18:53 -0500

    usando listview

--- proy_flutter/lib/views/profile_screen.dart
+++ proy_flutter/lib/views/profile_screen.dart
@@ -41,101 +41,105 @@ class _ProfileScreenState extends State<ProfileScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data:
-          ThemeData(primaryIconTheme: const IconThemeData(color: Colors.white)),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-        ),
-        backgroundColor: Colors.white,
-        drawer: const customDrawerW(),
-        extendBodyBehindAppBar: true,
-        body: Column(
-          children: [
-            SizedBox(
-              height: 320,
-              child: Stack(alignment: Alignment.topCenter, children: [
-                SizedBox(
-                  height: 180,
-                  width: double.infinity,
-                  child: Image.network(
-                    'https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274__340.jpg',
-                    fit: BoxFit.cover,
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Perfil'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      drawer: const customDrawerW(),
+      extendBodyBehindAppBar: true,
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 320,
+            child: Stack(alignment: Alignment.topCenter, children: [
+              SizedBox(
+                height: 180,
+                width: double.infinity,
+                child: Image.network(
+                  'https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274__340.jpg',
+                  fit: BoxFit.cover,
                 ),
-                Positioned(
-                  bottom: 0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.white, width: 12)),
-                        child: const CircleAvatar(
-                            radius: 50,
-                            backgroundImage: NetworkImage(
-                                'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')),
-                      ),
-                      const Text(
-                        'Chris Yovanka',
+              ),
+              Positioned(
+                bottom: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 12)),
+                      child: const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                              'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')),
+                    ),
+                    const Text(
+                      'Chris Yovanka',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        'Designer | Photographer',
                         style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black45),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 5),
-                        child: Text(
-                          'Designer | Photographer',
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.black45,
+                          size: 18,
+                        ),
+                        Text(
+                          'Lima, Perú',
                           style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
                               color: Colors.black45),
                         ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ]),
+          ),
+          Container(
+            height: 350.0,
+            child: Column(
+              children: [
+                TabBar(
+                    labelColor: Colors.black,
+                    indicatorColor: Colors.purple,
+                    tabs: const [
+                      Tab(
+                        text: 'Diseños',
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.location_on,
-                            color: Colors.black45,
-                            size: 18,
-                          ),
-                          Text(
-                            'Lima, Perú',
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black45),
-                          ),
-                        ],
-                      ),
+                      Tab(
+                        text: 'Favoritos',
+                      )
                     ],
-                  ),
-                )
-              ]),
+                    controller: _tabController),
+                Expanded(
+                  child: TabBarView(controller: _tabController, children: [
+                    GridImageWidget(
+                        images: images, onTapImage: (id) => _onTapImage(id)),
+                    Text('Favoritos'),
+                  ]),
+                ),
+              ],
             ),
-            TabBar(
-                labelColor: Colors.black,
-                indicatorColor: Colors.purple,
-                tabs: const [
-                  Tab(
-                    text: 'Diseños',
-                  ),
-                  Tab(
-                    text: 'Favoritos',
-                  )
-                ],
-                controller: _tabController),
-            Expanded(
-              child: TabBarView(controller: _tabController, children: [
-                GridImageWidget(
-                    images: images, onTapImage: (id) => _onTapImage(id)),
-                Text('Favoritos'),
-              ]),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
