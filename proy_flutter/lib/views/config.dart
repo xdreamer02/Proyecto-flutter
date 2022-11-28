15fa3087a3c067ee3fcaef0c677af4b070ca227a
PercyVf <perci_tauro_3@hotmail.com>
2022-11-28 13:31:45 -0500

    Ajustes
    Ajuste Estatico

--- proy_flutter/lib/views/config.dart
+++ proy_flutter/lib/views/config.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:proy_flutter/preferences/preferences.dart';
 import 'package:proy_flutter/providers/theme_provider.dart';
 import 'package:proy_flutter/widgets/custom_Drawer.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class ConfigPage extends StatefulWidget {
   const ConfigPage({super.key});
@@ -16,7 +17,7 @@ class _ConfigPageState extends State<ConfigPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Configución'),
+        title: const Text('Configuración'),
         centerTitle: true,
         actions: [
           Switch.adaptive(
@@ -30,6 +31,85 @@ class _ConfigPageState extends State<ConfigPage> {
               }))
         ],
       ),
+      body:   SettingsList( 
+      sections: [
+        SettingsSection(
+          title: Text('Common', style: TextStyle(color: Colors.lightBlue),),
+          tiles: <SettingsTile>[
+            SettingsTile.navigation(
+              leading: Icon(Icons.language),
+              title: Text('Language'),
+              value: Text('Español'),
+            ),
+          
+             SettingsTile.navigation(
+              leading: Icon(Icons.cloud_outlined),
+              title: Text('Environment'),
+              value: Text('Production'),
+             
+            ),
+              SettingsTile.navigation(
+             
+              leading: Icon(Icons.devices_other),
+              title: Text('Platform'),
+              value: Text('Default'),
+          
+             
+        
+             
+            ),
+
+              SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: true,
+              leading: Icon(Icons.notifications_on),
+              title: Text('Enable notifications'),
+             
+              
+            ),
+           SettingsTile(
+title: Text('Account', style: TextStyle(color: Colors.lightBlue, fontSize: 15),),
+           ),
+              SettingsTile.navigation(
+              leading: Icon(Icons.phone),
+              title: Text('Phone number'),
+              
+             
+            ),
+               SettingsTile.navigation(
+              leading: Icon(Icons.email),
+              title: Text('Email'),
+               ),
+            
+              SettingsTile(
+title: Text('Security', style: TextStyle(color: Colors.lightBlue, fontSize: 15),),
+           ),
+            
+              SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: true,
+              leading: Icon(Icons.lock),
+              title: Text('Change password'),
+              ),
+              SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: false,
+              leading: Icon(Icons.fingerprint),
+              title: Text('Use fingerprint'),
+              ),
+              
+            
+           
+           ],
+        ),
+        
+        
+      ],
+      
+    ),
+    
+    
+
       drawer: customDrawerW(),
     );
   }
